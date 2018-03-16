24389f208dcd302a69acb839bb6f526e7aad3a64
Silverio <gsilver@umich.edu>
2018-03-16 07:21:52 -0400

    clean up, remove unused dependencies

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 // set up ======================================================================
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8090; 				// set the port
+var port = process.env.PORT || 8080;
 var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
-// routes ======================================================================
+app.use(express.static('./public'));
+app.use(morgan('dev'));
 require('./routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
