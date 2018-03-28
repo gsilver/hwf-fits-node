ef4e9e71c1035cd341f3d6b6ba45d96398ca5f95
Silverio <gsilver@m-c02rd0prg8wl.local>
2018-03-28 11:43:50 -0400

    return only the ESB response body, not the headers, etc.

--- routes.js
+++ routes.js
@@ -47,7 +47,7 @@ module.exports = function (app) {
         if (error | response.statusCode === 401) {
         }
         console.log(body);
-        res.send(response);
+        res.send({'statusCode':response.statusCode, 'body':body});
       });
   });
 
@@ -78,86 +78,3 @@ module.exports = function (app) {
     res.sendFile(__dirname + '/public/main.html');
   });
 };
-
-
-
-// var request = require('request');
-// require('dotenv').config();
-//
-// module.exports = function (app) {
-//
-//   app.get('/api/tokens/:scope', function (req, res) {
-//     console.log('getting the token for ' + req.params.scope);
-//     var options = {
-//       method: 'POST',
-//       url: process.env.APISERVER  + '/um/aa/oauth2/token',
-//       headers:
-//       {
-//         accept: 'application/json',
-//         'content-type': 'application/x-www-form-urlencoded'
-//       },
-//       form:{
-//         grant_type: 'client_credentials',
-//         client_id: process.env.CLIENTID,
-//         client_secret: process.env.CLIENTSECRET,
-//         scope: req.params.scope
-//       }
-//     };
-//
-//     request(options, function (error, response, body, next) {
-//       if (error){
-//         return console.error('Failed: %s', error.message);
-//       }
-//       else {
-//         var bodyObj = JSON.parse(body);
-//         app.locals[req.params.scope + '_token'] = bodyObj.access_token;
-//         res.send('ok');
-//       }
-//     });
-//   });
-//
-//   app.get('/api/courses/:term_id/:user_id', function (req, res) {
-//       var options = {
-//         method: 'GET',
-//         url: process.env.APISERVER  + '/um/aa/Instructors/' + req.params.user_id + '/Terms/' + req.params.term_id + '/Classes',
-//         headers:  {
-//           accept: 'application/json',
-//           authorization: 'Bearer ' + app.locals.instructors_token,
-//           'x-ibm-client-id': process.env.CLIENTID
-//           }
-//         };
-//       request(options, function (error, response, body) {
-//         if (error | response.statusCode === 401) {
-//         }
-//         console.log();
-//         res.send(response);
-//       });
-//   });
-//
-//
-//   app.get('/data/location/buildings.json', function (req, res) {
-//     res.sendFile(__dirname + '/public/js/data/location/buildings.json');
-//   });
-//
-//   app.get('/api/course/:term_id/:course_id', function (req, res) {
-//     //var token = getToken();
-//     var options = {
-//       method: 'GET',
-//       url: process.env.APISERVER  + '/um/Curriculum/SOC/Terms/' + req.params.term_id + '/Classes/' + req.params.course_id,
-//       headers: {
-//         accept: 'application/json',
-//         authorization: 'Bearer '  + app.locals.umscheduleofclasses_token,
-//         'x-ibm-client-id': process.env.CLIENTID
-//       }
-//     };
-//
-//     request(options, function (error, response, body) {
-//       if (error) return console.error('Failed: %s', error.message);
-//       res.send(response);
-//     });
-//   });
-//
-//   app.get('/', function (req, res) {
-//     res.sendFile(__dirname + '/public/main.html');
-//   });
-// };
